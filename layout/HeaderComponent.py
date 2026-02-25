9f99be877b7400a23b7dd0bed8da4aaca03ce236
Vinicius Vasconcelos <63874530+viniciusvasconcelos9@users.noreply.github.com>
2026-02-25 00:31:53 -0300

    Update HeaderComponent.py

--- layout/HeaderComponent.py
+++ layout/HeaderComponent.py
@@ -10,7 +10,7 @@ def HeaderComponent():
     st.title("📊 Análise Climática Comparativa")
     st.write("Dados - 2025-2026")
     st.write("Fonte: Instituto Nacional de Meteorologia, INMET - Estação A572")
-    st.write("Estação: BELO HORIZONTE - SANTO AGOSTINHO")
+    st.write("Estação: SANTO AGOSTINHO - BELO HORIZONTE/MG")
     st.write("Localização: -19.9338, -43.9522")
     st.markdown("_Tem algumas inconsistências por falta de dados do ano_")
     st.markdown("*Feito por Vinícius Vasconcelos*")
